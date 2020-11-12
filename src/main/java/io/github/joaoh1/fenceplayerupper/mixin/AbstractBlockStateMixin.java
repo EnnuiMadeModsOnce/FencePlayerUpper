cd3cd8ae088280343e979e5667763628dbac2801
joaoh1 <7559492+joaoh1@users.noreply.github.com>
2020-11-11 22:14:21 -0300

    Let allowed entities pathfind III: 1.1.0

--- src/main/java/io/github/joaoh1/fenceplayerupper/mixin/AbstractBlockStateMixin.java
+++ src/main/java/io/github/joaoh1/fenceplayerupper/mixin/AbstractBlockStateMixin.java
@@ -1,3 +1,21 @@
+/*
+ * Fence Player Upper
+ * Copyright (C) 2020 joaoh1
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.joaoh1.fenceplayerupper.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,7 +32,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 
 @Mixin(AbstractBlockState.class)
-public class MobNavigationMixin {
+public class AbstractBlockStateMixin {
     @Inject(
         at = @At("RETURN"),
         method = "canPathfindThrough",
