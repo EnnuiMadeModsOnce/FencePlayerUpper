48460a1876de671b59412fb5b8c93bbdf1fae768
joaoh1 <7559492+joaoh1@users.noreply.github.com>
2021-02-08 13:53:57 -0300

    Potential 1.1.2 Release

--- src/main/java/io/github/joaoh1/fenceplayerupper/utils/UpperUtils.java
+++ src/main/java/io/github/joaoh1/fenceplayerupper/utils/UpperUtils.java
@@ -1,6 +1,6 @@
 /*
  * Fence Player Upper
- * Copyright (C) 2020 joaoh1
+ * Copyright (C) 2020-2021 joaoh1
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,10 +20,12 @@ package io.github.joaoh1.fenceplayerupper.utils;
 
 import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityType;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
 public class UpperUtils {
@@ -31,25 +33,14 @@ public class UpperUtils {
 	public static final Tag<EntityType<?>> ALLOWED_ENTITIES = TagRegistry.entityType(new Identifier("fenceplayerupper", "allowed_entities"));
 
 	public static final boolean canJumpFence(World world, BlockPos pos) {
-		return world.getBlockState(pos).isIn(BOOST_JUMP) && !world.getBlockState(pos).getCollisionShape(world, pos).isEmpty();
+		BlockState blockState = world.getBlockState(pos);
+		return blockState.isIn(BOOST_JUMP) && !blockState.getCollisionShape(world, pos).isEmpty();
 	}
 
 	public static final BlockPos[] createFencePosArray(BlockPos pos, double x, double z) {
-		boolean reduceRange = false;
-		if ((x == 0.75 || x == -0.75) && (z == 0.75 || z == -0.75)) {
+		if (MathHelper.abs((float) x) == 0.75f && MathHelper.abs((float) z) == 0.75f) {
 			x = (x / 3 * 4);
 			z = (z / 3 * 4);
-			reduceRange = true;
-		}
-		if (!reduceRange) {
-			return new BlockPos[] {
-				pos,
-				pos.add(x, 0, z),
-				pos.add(x * 2, 0, z * 2),
-				pos.add(x * 3, 0, z * 3),
-				pos.add(-x, 0, -z)
-			};
-		} else {
 			return new BlockPos[] {
 				pos,
 				pos.add(x, 0, z),
@@ -57,5 +48,13 @@ public class UpperUtils {
 				pos.add(-x, 0, -z)
 			};
 		}
+
+		return new BlockPos[] {
+			pos,
+			pos.add(x, 0, z),
+			pos.add(x * 2, 0, z * 2),
+			pos.add(x * 3, 0, z * 3),
+			pos.add(-x, 0, -z)
+		};
 	}
 }
