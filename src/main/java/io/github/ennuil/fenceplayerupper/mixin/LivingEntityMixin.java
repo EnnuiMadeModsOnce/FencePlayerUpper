34694785098b9bc3ebf334aa308ce945363713a1
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2021-12-15 17:15:50 -0300

    Begin 1.18 update, attempt to find better algo and i failed

--- src/main/java/io/github/ennuil/fenceplayerupper/mixin/LivingEntityMixin.java
+++ src/main/java/io/github/ennuil/fenceplayerupper/mixin/LivingEntityMixin.java
@@ -3,6 +3,8 @@ package io.github.ennuil.fenceplayerupper.mixin;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -32,27 +34,38 @@ public abstract class LivingEntityMixin extends Entity {
 				BlockPos currentPos = this.getBlockPos();
 				BlockPos[] positionsToCheck = UpperUtils.createFencePosArray(
 					currentPos,
-					Math.round(this.getRotationVector(0, this.getYaw()).getX() * 4.0) / 4.0,
-					Math.round(this.getRotationVector(0, this.getYaw()).getZ() * 4.0) / 4.0
+					this.getVelocity().multiply(20.0).getX(),
+					this.getVelocity().multiply(20.0).getZ()
 				);
+
 				boolean boostJump = false;
 				for (BlockPos blockPos : positionsToCheck) {
-					// this.world.addParticle(ParticleTypes.ANGRY_VILLAGER, blockPos.getX() + 0.5, blockPos.getY() - 0.5, blockPos.getZ() + 0.5, 0, 0, 0);
+					//this.world.addParticle(ParticleTypes.ANGRY_VILLAGER, blockPos.getX() + 0.5, blockPos.getY() - 0.5, blockPos.getZ() + 0.5, 0, 0, 0);
 					if (UpperUtils.canJumpFence(this.world, blockPos)) {
 						boostJump = true;
 						break;
 					} else if (!this.world.getBlockState(blockPos).getCollisionShape(this.world, blockPos).isEmpty()) {
+						this.world.addParticle(ParticleTypes.EXPLOSION, blockPos.getX() + 0.5, blockPos.getY() - 0.5, blockPos.getZ() + 0.5, 0, 0, 0);
 						break;
 					}
 				}
-				if (boostJump) {
-					if (!this.world.isClient) {
-						originalJumpVelocity -= 0.03;
+
+				if ((LivingEntity)(Object)this instanceof ServerPlayerEntity) {
+					System.out.println("Server: " + boostJump);
+					System.out.println(this.getVelocity());
+					if (boostJump) {
+						if (!this.world.isClient) {
+							originalJumpVelocity -= 0.03;
+						}
+						this.velocityModified = true;
 					}
-					this.velocityModified = true;
+				} else {
+					System.out.println("Client: " + boostJump);
+					System.out.println(this.getVelocity());
 				}
 			}
 		}
+
 		return originalJumpVelocity;
 	}
 }
